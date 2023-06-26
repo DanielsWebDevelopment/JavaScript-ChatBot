a68b2b17e78f32ecb747ff96ed41221c0a3a333c
Daniel <daniel23504209@gmail.com>
2023-06-26 08:52:40 -0700

    Clone Project Chatbot

---
+++ main.js
@@ -0,0 +1,49 @@
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+
+let userMessage;
+const API_KEY = "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";
+
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing"? `<p>${message}</p>` : `<span id="send-btn"><i class="fa-solid fa-person"></i></span><p>${message}</p>`;
+    chatLi.innerHTML = chatContent;
+    return chatLi;
+}
+
+const generateResponse = () => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "system", "content": userMessage}]
+        })
+    }
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        console.log(data);
+    }).catch((error) => {
+        console.log(error);
+    })
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if(!userMessage) return;
+
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+
+    setTimeout(() => {
+        chatbox.appendChild(createChatLi("Thinking...", "incoming"));
+        generateResponse();
+    }, 600);
+}
+
+sendChatBtn.addEventListener("click", handleChat);
